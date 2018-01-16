07570d99f9a49ab55ce34c0d30ad13ed097458ad
Dima Maiboroda <dimayboroda@gmail.com>
2018-01-17 00:37:44 +0200

    3 days

--- dist/js/common.js
+++ dist/js/common.js
@@ -7,4 +7,24 @@ $(document).ready(function(){
       $("#header").removeClass("main-header--scroll").fadeIn("fast");
     };
   });
+
+
+// Відкриття попапа нажаттям на .modal-open
+  $('.feedback__btn').click(function() {
+    $('.overlay').fadeIn(100);
+    $("body").addClass("body-inactive");
+  });
+// Закриття попапа нажаттям на хрестик
+  $('.close').click(function() {
+    $('.overlay').fadeOut(100);
+    $("body").removeClass("body-inactive");
+  });  
+// Закриття попапа нажаттям на "сіру зону"
+  $(document).mouseup(function(e){
+    var pop = $('.popup');
+    if(e.target!=pop[0]&&pop.has(e.target).length === 0) {
+      $('.overlay').fadeOut(100);
+      $("body").removeClass("body-inactive");
+    }
+  });
 });
